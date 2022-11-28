e5a9a9c48f850044b55f462e10afc1524aeb01dc
ValereCode <setodji2001@gmail.com>
2022-11-28 16:35:41 +0000

    Pour télécharger playliste ou vidéo

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 from pytube import YouTube
 from pytube import Playlist
 
-p_url = "https://www.youtube.com/watch?v=PAvn_jBNVNE&list=PLFMhxiCgmMR9Pu0v-VjNdEaRLcoUqHLFT"  # url d'un playlist
-
-
+response = input("What do you want to download(1=video or 2=playlist) ")
 
 def progression(stream, chunk, byte_remaining):
     """
@@ -20,15 +18,25 @@ def progression(stream, chunk, byte_remaining):
     print("Progression = ", int(pourcentage), "%")
 
 
-youtube_playlist = Playlist(p_url)
-print(youtube_playlist.video_urls)
-i = 1
-for url in youtube_playlist.video_urls:
+if int (response) == 1:
+    url = input("Enter the url of the video ")
     youtube_video = YouTube(url)  # La video correspondant à l'url précédent
     youtube_video.register_on_progress_callback(progression)
     stream = youtube_video.streams.get_highest_resolution()
-    print("Téléchargement", i, "...")
-    i += 1
+    print("Téléchargement...")
     stream.download()
+
+else:
+    p_url = input("Enter the url of the video ")
+    youtube_playlist = Playlist(p_url)
+    print(youtube_playlist.video_urls)
+    i = 1
+    for url in youtube_playlist.video_urls:
+        youtube_video = YouTube(url)  # La video correspondant à l'url précédent
+        youtube_video.register_on_progress_callback(progression)
+        stream = youtube_video.streams.get_highest_resolution()
+        print("Téléchargement", i, "...")
+        i += 1
+        stream.download()
 
 print("OK")
